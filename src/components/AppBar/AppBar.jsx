b0174c61cbb603007f5238ce447db3227effeec5
Kateryna <kati.skrinnikova@gmail.com>
2024-11-04 11:34:53 +0200

    added fonts

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,13 +1,16 @@
 import Logo from "../Logo/Logo";
 import Navigation from "../Navigation/Navigation";
 import AuthNav from "../AuthNav/AuthNav";
+import css from "../../components/AppBar/AppBar.module.css";
 
 export default function AppBar() {
   return (
-    <header>
-      <Logo />
-      <Navigation />
-      <AuthNav />
+    <header className="container">
+      <div className={css.wrapper}>
+        <Logo />
+        <Navigation />
+        <AuthNav />
+      </div>
     </header>
   );
 }

71e339794af1e448170e441415695b5084e803ae
Kateryna <kati.skrinnikova@gmail.com>
2024-11-27 16:01:43 +0200

    registrationForm css

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,5 +1,7 @@
 import { useForm } from "react-hook-form";
 import Button from "../Button/Button";
+import css from "../RegistrationForm/RegistrationForm.module.css";
+import sprite from "../../images/sprite.svg";
 
 export default function RegisterForm() {
   const {
@@ -13,33 +15,55 @@ export default function RegisterForm() {
   };
 
   return (
-    <div>
-      <h2>Registration</h2>
-      <p>
-        Thank you for your interest in our platform! In order to register, we
-        need some information. Please provide us with the following information
-      </p>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("name", { maxLength: 10 })} placeholder="Name" />
-        {errors.name && <span>The name more then 10</span>}
+    <div className={css.backdrop}>
+      <div className={css.wrapper}>
+        <h2 className={css.title}>Registration</h2>
+        <p className={css.text}>
+          Thank you for your interest in our platform! In order to register, we
+          need some information. Please provide us with the following
+          information
+        </p>
+        <button className={css.btnClose}>
+          <svg className={css.iconClose}>
+            <use href={`${sprite}#icon-x`}></use>
+          </svg>
+        </button>
+        <form className={css.form} onSubmit={handleSubmit(onSubmit)}>
+          <div className={css.wrap}>
+            <input
+              className={css.input}
+              {...register("name", { maxLength: 10 })}
+              placeholder="Name"
+            />
+            {errors.name && (
+              <span className={css.error}>The name more then 10</span>
+            )}
 
-        <input
-          {...register("email", {
-            required: true,
-            pattern: /^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/,
-          })}
-          placeholder="Email"
-        />
-        {errors.email && <span>Email is wrong</span>}
+            <input
+              className={css.input}
+              {...register("email", {
+                required: true,
+                pattern: /^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/,
+              })}
+              placeholder="Email"
+            />
+            {errors.email && <span className={css.error}>Email is wrong</span>}
 
-        <input
-          {...register("password", { required: true })}
-          placeholder="Password"
-        />
-        {errors.password && <span>Enter the password</span>}
+            <input
+              className={css.input}
+              {...register("password", { required: true })}
+              placeholder="Password"
+            />
+            {errors.password && (
+              <span className={css.error}>Enter the password</span>
+            )}
+          </div>
 
-        <Button type="submit">Sign Up</Button>
-      </form>
+          <Button className={css.button} type="submit">
+            Sign Up
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
